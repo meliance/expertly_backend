49ac060875dbffc52e55c3e4f8be4762eea0695c
Meliance <melakmu66@gmail.com>
2025-04-11 19:56:28 +0300

    gunicorn added

--- expertly/wsgi.py
+++ expertly/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expertly.expertly.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expertly.settings")
 
 application = get_wsgi_application()
