a765bc1be3c09e193f7bb152a2325d1773b506a3
Meliance <melakmu66@gmail.com>
2025-04-11 16:41:32 +0300

    setting.py edited

--- expertly/expertly/settings.py
+++ expertly/expertly/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False".lower()) == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 
 # Application definition
 
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "django_filters",
-    ""
     "api",
     "accounts",
     "documents",
@@ -91,8 +90,8 @@ DATABASES = {
     }
 }
 database_url = os.environ.get("DATABASE_URL")
-
-DATABASES["default"] = dj_database_url.parse("database_url")
+if database_url:
+    DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
